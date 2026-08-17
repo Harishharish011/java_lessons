6f6d7b08da97f1dc452a7cead68aa7d3942b16aa
HARISH <vkharish011@gmail.com>
2026-08-17 12:03:36 +0530

    Update: Changes in Hashmap

--- Advanced01/hashmap.java
+++ Advanced01/hashmap.java
@@ -6,6 +6,9 @@ public class hashmap {
         Details.put("Name","Harish");
         Details.put("Age","20");
         Details.put("Gender","Male");   
+        Details.put("Name","Arun");
+        Details.put("Name","vibin"); //First duplicate value will be replaced by the last duplicate value
         System.out.println(Details);
+        System.out.println(Details.get("Gender"));
     }
 }
