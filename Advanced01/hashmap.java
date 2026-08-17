fe3c53dbcde8cc4837f47392740aa307ea5168bb
HARISH <vkharish011@gmail.com>
2026-08-17 11:55:50 +0530

    Lesson: HashMap

---
+++ Advanced01/hashmap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class hashmap {
+    public static void main(String[]args) {
+        HashMap <String,String> Details = new HashMap<>();
+        Details.put("Name","Harish");
+        Details.put("Age","20");
+        Details.put("Gender","Male");   
+        System.out.println(Details);
+    }
+}
