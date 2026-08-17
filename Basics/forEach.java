f558b2c92ea6d086d4bf4c4eab3dfb90259428b7
HARISH <vkharish011@gmail.com>
2026-08-17 11:08:07 +0530

    ForEach looping

---
+++ Basics/forEach.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class forEach {
+    public static void main(String[]args) {
+        int[] List = {1,2,3,4,5};
+        for (int lst: List) {
+            System.out.println(lst);
+        }
+    }
+}
